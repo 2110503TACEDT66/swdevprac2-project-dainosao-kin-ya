6555e0acfe41c309e71946eb3b8f7031936f53c2
Wassamont <nene.nesaki@gmail.com>
2024-03-25 11:19:12 +0700

    pust interface

--- interfaces.ts
+++ interfaces.ts
@@ -7,3 +7,14 @@ export interface ReservationItem {
     returnDate: string,
     returnLocation: string
 }
+
+export interface Hotel {
+    name: String,
+    address: String,
+    district: String,
+    province: String,
+    postalcode: String,
+    tel: String,
+    region: String,
+    picture: String
+}
