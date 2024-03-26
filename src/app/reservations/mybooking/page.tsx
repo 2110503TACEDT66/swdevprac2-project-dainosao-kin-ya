aabe37941080953cd3a8328c02b09b3e25f67ac6
Pariya Vimolchalao <imppariya@Pariyas-MacBook-Air.local>
2024-03-26 12:12:33 +0700

    my booking

---
+++ src/app/reservations/mybooking/page.tsx
@@ -0,0 +1,9 @@
+import BookingList from "@/components/BookingList";
+
+export default function ManageReservations(){
+    return(
+        <main>
+            <BookingList/>
+        </main>
+    )
+}
