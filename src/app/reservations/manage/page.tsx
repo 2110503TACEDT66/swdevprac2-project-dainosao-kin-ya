d85cbcf1b2f53161a88a827f5920ba218530cfd8
Pariya Vimolchalao <imppariya@Pariyas-MacBook-Air.local>
2024-03-26 12:06:28 +0700

    BookingList and User

--- src/app/reservations/manage/page.tsx
+++ src/app/reservations/manage/page.tsx
@@ -1,7 +1,6 @@
 export default function ManageReservations(){
     return(
         <main>
-            <div className="text-center text-lg">Manage Your Reservation</div>
         </main>
     )
 }
---
+++ src/app/reservations/mybooking/page.tsx
@@ -0,0 +1,11 @@
+import BookingList from "@/components/BookingList";
+import User from "@/components/User";
+
+export default function ManageReservations(){
+    return(
+        <main>
+            
+            <BookingList/>
+        </main>
+    )
+}
