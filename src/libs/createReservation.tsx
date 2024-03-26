39d7145bde596fc8c5c9d43b7ce597d7ca781e08
Wassamont <nene.nesaki@gmail.com>
2024-03-26 10:50:12 +0700

    make reserve

---
+++ src/libs/createReservation.tsx
@@ -0,0 +1,21 @@
+import { ReservationItem } from "interfaces"
+
+export default async function createReservation(token: string,reserveItem: ReservationItem) {
+    const response = await fetch(`http://localhost:5000/api/v1/hotels/${reserveItem.hotelId}/reservations/`,{
+        method : "POST",
+        headers: {
+            authorization : `Bearer ${token}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            revDate: reserveItem.revDate,
+            nightNum: reserveItem.nightNum
+        }),
+    })
+
+    if(!response.ok){
+        console.log(response.status)
+        throw new Error("Failed to create reservation")
+    }
+    return await response.json()
+}
